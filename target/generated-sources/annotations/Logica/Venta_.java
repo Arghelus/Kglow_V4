93b6d391ee3fddaf323e44fbd9834d3adbaf2668
Airton Rodriguez <>
2024-12-03 23:19:19 -0500

    agregando clases y pruebas

--- target/generated-sources/annotations/Logica/Venta_.java
+++ target/generated-sources/annotations/Logica/Venta_.java
@@ -3,7 +3,7 @@ package Logica;
 import javax.annotation.processing.Generated;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2024-10-31T12:29:48", comments="EclipseLink-2.7.12.v20230209-rNA")
+@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2024-12-03T23:16:35", comments="EclipseLink-2.7.12.v20230209-rNA")
 @StaticMetamodel(Venta.class)
 public class Venta_ { 
 
